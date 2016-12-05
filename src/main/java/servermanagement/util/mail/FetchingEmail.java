0cc2c1c606e108280c868b59bc7bcfd2e92617f1
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2016-12-05 21:34:25 +0400

    Added mail fetcher, and credentials file

---
+++ src/main/java/servermanagement/util/mail/FetchingEmail.java
@@ -0,0 +1,144 @@
+package main.java.servermanagement.util.mail;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.*;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FetchingEmail {
+    /** Application name. */
+    private static final String APPLICATION_NAME = "Gmail API";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/gmail-api");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/gmail-java-quickstart
+     */
+    private static final List<String> SCOPES = Arrays.asList(GmailScopes.MAIL_GOOGLE_COM);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Gmail service = getGmailService();
+        String user = "me";
+
+        ListMessagesResponse messagesResponse = service.users().messages().list(user)
+                .setQ("is:unread in:inbox from:aram.bekverdyan@gmail.com").execute();
+
+        List<Message> messages = messagesResponse.getMessages();
+
+        List<String> messageIds = messages.stream().map(Message::getId).collect(Collectors.toList());
+
+        Message msg;
+
+        for (String messageId : messageIds) {
+            msg = service.users().messages().get(user, messageId).setFormat("raw").execute();
+            byte[] emailBytes = msg.decodeRaw();
+            String messageContent = new String(emailBytes, "UTF-8");
+
+            List<String> wholeMail = Arrays.asList(messageContent.split("\\r?\\n"));
+
+            List<String> content = wholeMail.subList(wholeMail.indexOf("***BEGIN***") + 1, wholeMail.indexOf("***END***"));
+
+            //
+
+            System.out.println(content.toString());
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+                FetchingEmail.class.getResourceAsStream("/client_secret_gmail-api.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Gmail client service.
+     * @return an authorized Gmail client service
+     * @throws IOException
+     */
+    public static Gmail getGmailService() throws IOException {
+        Credential credential = authorize();
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void getAttachments(Gmail service, String userId, String messageId)
+            throws IOException {
+        Message message = service.users().messages().get(userId, messageId).execute();
+        List<MessagePart> parts = message.getPayload().getParts();
+        for (MessagePart part : parts) {
+            if (part.getFilename() != null && part.getFilename().length() > 0) {
+                String filename = part.getFilename();
+                String attId = part.getBody().getAttachmentId();
+                MessagePartBody attachPart = service.users().messages().attachments().get(userId, messageId, attId).execute();
+
+                Base64 base64url = new Base64(true);
+                byte[] fileByteArray = base64url.decodeBase64(attachPart.getData());
+                FileOutputStream fileOutFile =
+                        new FileOutputStream("/home/aram/Pictures/" + filename);
+                fileOutFile.write(fileByteArray);
+                fileOutFile.close();
+            }
+        }
+    }
+}

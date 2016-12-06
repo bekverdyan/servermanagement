4aa711c028f4f17748c30fd7027338711baeaa4d
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2016-12-06 18:07:46 +0400

    1. Added markaAsReadAndArchieveTheEmail and listLabels methods. 2. Modified checkForEmail method.

--- src/main/java/servermanagement/util/mail/FetchingEmail.java
+++ src/main/java/servermanagement/util/mail/FetchingEmail.java
@@ -9,19 +9,40 @@ import java.util.stream.Collectors;
 import main.java.servermanagement.util.ConfigUtils;
 import model.ConfigModel;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONObject;
 
 import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
 import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
 import com.google.api.services.gmail.model.MessagePart;
 import com.google.api.services.gmail.model.MessagePartBody;
+import com.google.api.services.gmail.model.ModifyMessageRequest;
 
 public class FetchingEmail {
+  private static final String user = "me";
+
+  public static void listLabels() throws IOException {
+    Gmail service = GmailServiceBuilder.INSTANCE.getGmailService();
+    ListLabelsResponse response = service.users().labels().list(user).execute();
+    List<Label> labels = response.getLabels();
+    for (Label label : labels) {
+      System.out.println(label.toPrettyString());
+    }
+  }
+
+  private Message markaAsReadAndArchieveTheEmail(Gmail service, String emailId) throws IOException {
+    ModifyMessageRequest mods = new ModifyMessageRequest().setAddLabelIds(Arrays.asList("Label_2"))
+        .setRemoveLabelIds(Arrays.asList("UNREAD", "INBOX"));
+
+    return service.users().messages().modify(user, emailId, mods).execute();
+  }
+
   public JSONObject checkForEmail() throws IOException {
     Gmail service = GmailServiceBuilder.INSTANCE.getGmailService();
-    String user = "me";
     ConfigModel configModel = ConfigUtils.loadConfigJson();
 
     ListMessagesResponse messagesResponse = service.users().messages().list(user)
@@ -34,30 +55,33 @@ public class FetchingEmail {
 
     for (String messageId : messageIds) {
       msg = service.users().messages().get(user, messageId).setFormat("raw").execute();
-      byte[] emailBytes = msg.decodeRaw();
-      String messageContent = new String(emailBytes, "UTF-8");
+      String messageContentRaw = msg.getSnippet();
 
-      List<String> wholeMail = Arrays.asList(messageContent.split("\\r?\\n"));
+      List<String> messageContent = Arrays.asList(messageContentRaw.split(","));
 
-      List<String> content = wholeMail.subList(wholeMail.indexOf("***BEGIN***") + 1, wholeMail.indexOf("***END***"));
-
-      for (String rawObject : content) {
+      for (String rawObject : messageContent) {
         try {
+          rawObject = StringUtils.removeStart(rawObject, " ");
+          rawObject = StringUtils.removeEnd(rawObject, " ");
           parseAndPutIntoJson(jsonObject, rawObject);
         } catch (Exception e) {
           e.printStackTrace();
         }
       }
+
+      markaAsReadAndArchieveTheEmail(service, messageId);
     }
 
     return jsonObject;
   }
 
+  @SuppressWarnings("unchecked")
   private void parseAndPutIntoJson(JSONObject jsonObject, String raw) throws Exception {
     String[] splitted = raw.split("\\s+");
     jsonObject.put(splitted[0], splitted[1]);
   }
 
+  @SuppressWarnings("static-access")
   public void getAttachments(Gmail service, String userId, String messageId) throws IOException {
     Message message = service.users().messages().get(userId, messageId).execute();
     List<MessagePart> parts = message.getPayload().getParts();
